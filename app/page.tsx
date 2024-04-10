8b487c86761666456f3ce7b53b0625d01ffce3bc
Peter Pardo <peterpardo123@gmail.com>
2024-04-10 12:41:32 +0800

    feat: setup layout for app

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <main>Hello World!</main>;
+  return (
+    <main className="h-screen flex">
+      <div className="bg-slate-300 min-w-20">sidebar 1</div>
+      <div className="bg-slate-200 min-w-24 max-w-[360px] w-full">
+        sidebar 2
+      </div>
+      <div className="bg-slate-100 w-full">chat</div>
+    </main>
+  );
 }
